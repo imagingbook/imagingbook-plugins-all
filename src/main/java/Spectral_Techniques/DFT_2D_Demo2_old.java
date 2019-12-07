86f26b427203a9c2e580d924670a327c7d4d7c12
imagingbook <wilhelm.burger@fh-hagenberg.at>
2019-12-07 14:36:26 +0100

    fixing DFT_2D_Demo.java

--- src/main/java/Spectral_Techniques/DFT_2D_Demo2_old.java
+++ src/main/java/Spectral_Techniques/DFT_2D_Demo2_old.java
@@ -20,7 +20,7 @@ import imagingbook.pub.dft.Dft2d;
  * of arbitrary size.
  * TODO: adapt API to the DCT layout.
  */
-public class DFT_2D_Demo implements PlugInFilter{
+public class DFT_2D_Demo2_old implements PlugInFilter{
 
 	static boolean center = true;    //center the resulting spectrum?
 	
@@ -31,30 +31,11 @@ public class DFT_2D_Demo implements PlugInFilter{
 	public void run(ImageProcessor ip) {
 		IJ.log("runnung DFT_2D_Demo");
 		FloatProcessor fp = ip.convertToFloatProcessor();
-		
-		//Dft2d dft = new Dft2d(fp, center);
-		
-		double[][][] g = toComplexArray(fp);
-		Dft2d dft2D = new Dft2d(ip.getWidth(), ip.getHeight());
-		double[][][] G = dft2D.dft(g);
+		Dft2d dft = new Dft2d(fp, center);
 
-		ImageProcessor ipP = dft2D.makePowerImage();
+		ImageProcessor ipP = dft.makePowerImage();
 		ImagePlus win = new ImagePlus("DFT Power Spectrum (byte)", ipP);
 		win.show();
-	}
-	
-	private double[][][] toComplexArray(FloatProcessor fp) {
-		final int width = fp.getWidth();
-		final int height = fp.getHeight();
-		float[][] pixels = fp.getFloatArray(); 
-		double[][][] arr = new double[width][height][2];
-		for (int u = 0; u < width; u++) {
-			for (int v = 0; v < height; v++) {
-				arr[u][v][0] = pixels[u][v];
-				arr[u][v][1] = 0;
-			}
-		}
-		return arr;
 	}
 
 }
