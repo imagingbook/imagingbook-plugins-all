940d42893ee3747faf8ed43860dcf8ed984a7f7d
imagingbook <wilhelm.burger@fh-hagenberg.at>
2021-04-19 12:20:03 +0200

    working on region demos

--- src/main/java/Binary_Regions/Region_Ellipse_Demo.java
+++ src/main/java/Binary_Regions/Region_Ellipse_Demo.java
@@ -4,9 +4,13 @@ import static imagingbook.lib.math.Arithmetic.sqr;
 import static java.lang.Math.sqrt;
 
 import java.awt.Color;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
 import java.util.List;
+import java.util.Locale;
 
 import ij.IJ;
 import ij.ImagePlus;
@@ -18,17 +22,17 @@ import ij.plugin.filter.PlugInFilter;
 import ij.process.ByteProcessor;
 import ij.process.ImageProcessor;
 import imagingbook.lib.ij.IjUtils;
+import imagingbook.lib.math.Eigensolver2x2;
 import imagingbook.pub.geometry.basic.Pnt2d;
 import imagingbook.pub.regions.BinaryRegionSegmentation.BinaryRegion;
 import imagingbook.pub.regions.NeighborhoodType;
 import imagingbook.pub.regions.SegmentationRegionContour;
 
 /**
- * Displays each region's major axis as a vector scaled by the region's eccentricity.
+ * Displays each region's equivalent ellipse as a vector overlay.
  * Infinite eccentricity values (e.g., from straight lines) is limited to
  * {@link #MaxEccentricity} and marked red.
  * {@code NaN} eccentricity values (caused by single-pixel regions) are not displayed.
- * A vector overlay is used for graphics.
  * <br>
  * This plugin expects a binary (black and white) image with background = 0 and 
  * foreground &gt; 0.
@@ -36,7 +40,11 @@ import imagingbook.pub.regions.SegmentationRegionContour;
  * @author W. Burger
  * @version 2021/04/18
  */
-public class Region_Eccentricity_Demo implements PlugInFilter {
+public class Region_Ellipse_Demo implements PlugInFilter {
+	
+	static {
+		Locale.setDefault(Locale.US);
+	}
 	
 	private static NeighborhoodType NhT = NeighborhoodType.N4;
 	private static Color 	AxisColor = Color.green;
@@ -103,7 +111,11 @@ public class Region_Eccentricity_Demo implements PlugInFilter {
 			if (B < 0) {
 				throw new RuntimeException("negative B: " + B);
 			}
-			double ecc = (A + sqrt(B)) / (A - sqrt(B));
+			
+			double a1 = A + sqrt(B);		// see book eq. 10.34
+			double a2 = A - sqrt(B);
+			//double ecc = (A + sqrt(B)) / (A - sqrt(B));
+			double ecc = a1 / a2;
 					
 			if (Double.isNaN(ecc)) {	// single pixel region: A + sqrt(B) == A - sqrt(B) == 0
 				continue;
@@ -119,13 +131,31 @@ public class Region_Eccentricity_Demo implements PlugInFilter {
 			double dx = Math.cos(theta) * len;
 			double dy = Math.sin(theta) * len;
 			
-			//Roi roi = new Line(xc, yc, xc + dx, yc + dy);	// or 'Arrow'
-			
+						
 			Roi roi = new ShapeRoi(new Line2D.Double(xc, yc, xc + dx, yc + dy));
-			
 			roi.setStrokeWidth(AxisLineWidth);
 			roi.setStrokeColor(axisCol);
-			oly.add(roi);			
+			oly.add(roi);
+			
+			int n = r.getSize();
+			double ra1 = sqrt(2 * a1 / n);
+			double rb1 = sqrt(2 * a2 / n);
+			IJ.log(String.format("V1: ra=%.2f rb=%.2f", ra1, rb1));
+			
+			Roi re = makeEllipse(xc, yc, ra1, rb1, theta);
+			oly.add(re);
+			
+			// same result via Eigenvalues:
+			Eigensolver2x2 es = new Eigensolver2x2(mu20, mu11, mu11, mu02);
+			double lambda1 = es.getEigenvalue1();
+			double lambda2 = es.getEigenvalue2();
+			double ra2 = 2 * sqrt(lambda1 / n);
+			double rb2 = 2 * sqrt(lambda2 / n);
+			IJ.log(String.format("V2: ra=%.2f rb=%.2f", ra2, rb2));
+			IJ.log(String.format("    a1=%.2f   lam1=%.2f  2*lam1=%.2f", a1, lambda1, 2 * lambda1));
+			IJ.log(String.format("    a2=%.2f   lam2=%.2f  2*lam2=%.2f", a2, lambda2, 2 * lambda2));
+			
+			IJ.log("");
 		}
 		
 		im.setOverlay(oly);
@@ -151,6 +181,14 @@ public class Region_Eccentricity_Demo implements PlugInFilter {
 			mu11 = mu11 + dx * dy;
 		}
 		return new double[] {mu20, mu02, mu11};
+	}
+	
+	public Roi makeEllipse(double xc, double yc, double ra, double rb, double theta) {
+		Ellipse2D e = new Ellipse2D.Double(-ra, -rb, 2 * ra, 2 * rb);
+		AffineTransform t = new AffineTransform();
+		t.translate(xc, yc);
+		t.rotate(theta);
+		return new ShapeRoi(t.createTransformedShape(e));
 	}
 	
 	private Roi makeMarker(double x, double y, double r) {
