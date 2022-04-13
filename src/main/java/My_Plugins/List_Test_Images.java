5fe91c6c25a50e151bf2d3fb5916a738ff986943
imagingbook <wilbur@ieee.org>
2022-04-13 19:04:19 +0200

    testing resource stream from JAR file

--- src/main/java/My_Plugins/List_Test_Images.java
+++ src/main/java/My_Plugins/List_Test_Images.java
@@ -5,29 +5,29 @@ import java.net.URI;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.plugin.PlugIn;
-import imagingbook.testimages.MyImages;
+import imagingbook.core.resource.ImageResource;
+import imagingbook.testimages.RansacTestImage;
 
 public class List_Test_Images implements PlugIn {
 
 	@Override
 	public void run(String arg) {
-		for (MyImages mi : MyImages.values()) {
-			String relPath = mi.getRelativePath();
+		for (ImageResource res : RansacTestImage.values()) {
+			String relPath = res.getRelativePath();
 			IJ.log(relPath);
-//			URI uri = MyImages.getResourceUri(relPath);
-			URI uri = mi.getURI();
+
+			URI uri = res.getURI();
 			IJ.log("uri = " + uri);
-			IJ.log("inside JAR = " + mi.isInsideJar());
+			IJ.log("inside JAR = " + res.isInsideJar());
 			
 //			ImagePlus im = new Opener().openImage(uri.toString());
-			ImagePlus im = IJ.openImage(uri.toString());
+//			ImagePlus im = IJ.openImage(uri.toString());
+			ImagePlus im = res.getImage();
 			im.show();
 		}
-		
-//		MyImages res = MyImages.noisyCircles;
-//		res.getURI();
-//		URI uir = MyImages.noisyCircles.getURI();
-		
+
 	}
+	
+	
 
 }
---
+++ src/main/java/My_Plugins/Read_Icc_Profile.java
@@ -0,0 +1,86 @@
+package My_Plugins;
+
+import java.awt.color.ICC_ColorSpace;
+import java.awt.color.ICC_Profile;
+import java.io.IOException;
+import java.io.InputStream;
+
+import ij.IJ;
+import ij.plugin.PlugIn;
+import imagingbook.core.resource.NamedResource;
+import imagingbook.lib.color.IccProfile;
+import imagingbook.lib.math.Matrix;
+
+public class Read_Icc_Profile implements PlugIn {
+
+	@Override
+	public void run(String arg) {
+		NamedResource res = IccProfile.AdobeRGB1998;
+		IJ.log("URI = " + res.getURI());
+		IJ.log("inside JAR = " + res.isInsideJar());
+		
+		ICC_Profile profile = null;
+		InputStream strm = res.getStream();		// this must work when resource is in JAR!
+		try {
+			if (strm != null)
+				profile = ICC_Profile.getInstance(strm);
+		} catch (IOException e) { }
+		
+		IJ.log("profile = " + profile);
+		
+		ICC_ColorSpace iccColorSpace = new ICC_ColorSpace(profile);
+		int nComp = iccColorSpace.getNumComponents();
+		if (nComp != 3) {
+			IJ.error("Color space must have 3 components, this one has " + nComp);
+			return;
+		}
+		
+		IJ.log("color space = " + iccColorSpace);
+		IJ.log("color space type = " + iccColorSpace.getType());
+		IJ.log("color space ncomp = " + iccColorSpace.getNumComponents());
+		
+		
+		// specify a device-specific color:
+		float[] deviceColor = {0.77f, 0.13f, 0.89f};
+		//float[] deviceColor = {0.0f, 0.0f, 0.0f};
+		IJ.log("device color = " + Matrix.toString(deviceColor));
+		
+		// convert to sRGB:
+		float[] sRGBColor = iccColorSpace.toRGB(deviceColor);
+		IJ.log("sRGB = " + Matrix.toString(sRGBColor));
+		
+		// convert to (D50-based) XYZ:
+		float[] XYZColor = iccColorSpace.toCIEXYZ(deviceColor);
+		IJ.log("XYZ = " + Matrix.toString(XYZColor));
+				
+		deviceColor = iccColorSpace.fromRGB(sRGBColor);
+		IJ.log("device color direct (check) = " + Matrix.toString(deviceColor));
+		
+		deviceColor = iccColorSpace.fromCIEXYZ(XYZColor);
+		IJ.log("device color via XYZ (check) = " + Matrix.toString(deviceColor));
+		
+		// list sRGB Values:
+		for (int ri = 0; ri <= 10; ri++) {
+			for (int gi = 0; gi <= 10; gi++) {
+				for (int bi = 0; bi <= 10; bi++) {
+					float[] devCol = {ri * 0.1f, gi * 0.1f, bi * 0.1f};
+					float[] sRGB = iccColorSpace.toRGB(devCol);
+					float[] devColCheck = iccColorSpace.fromRGB(sRGB);
+					IJ.log(Matrix.toString(devCol) + " -> " + Matrix.toString(sRGB) + " -> " 
+							+ Matrix.toString(devColCheck) + warning(devCol, devColCheck));
+				}
+			}
+		}
+
+	}
+	
+	private String warning(float[] col1, float[] col2) {
+		float t = 0.05f;
+		for (int i = 0; i < col1.length; i++) {
+			if (Math.abs(col1[i] - col2[i]) > t)
+				return " ***";
+		}
+		return "";
+	}
+
+}
